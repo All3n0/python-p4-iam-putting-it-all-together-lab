1d61f113d8cc417c076135f2c60b9e2c70dd53bd
ALLAN <allankipr6@gmail.com>
2024-07-05 17:19:58 +0300

    lab done

---
+++ server/migrations/versions/23b6cf116f6b_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 23b6cf116f6b
+Revises: 
+Create Date: 2024-07-05 16:53:29.353039
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '23b6cf116f6b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
